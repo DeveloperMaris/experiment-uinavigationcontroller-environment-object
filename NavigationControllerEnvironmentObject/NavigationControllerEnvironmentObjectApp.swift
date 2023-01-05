4fcf592b14df3dc9efd90efafeff8ad87845961d
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2023-01-05 16:58:01 +0200

    Initial Commit

---
+++ NavigationControllerEnvironmentObject/NavigationControllerEnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationControllerEnvironmentObjectApp.swift
+//  NavigationControllerEnvironmentObject
+//
+//  Created by Maris Lagzdins on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationControllerEnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
